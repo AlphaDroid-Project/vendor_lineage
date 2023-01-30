f727dfbb351973d170c85d2b4b814b50cc69530d
elpaablo <donopwhile1@gmail.com>
2023-01-30 13:53:57 +0000

    restore OTA support

--- config/common.mk
+++ config/common.mk
@@ -111,11 +111,11 @@ endif
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard
-#   Updater
+    LineageSetupWizard \
+    Updater
 
-#PRODUCT_COPY_FILES += \
-#    vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
 # Themes
 PRODUCT_PACKAGES += \
