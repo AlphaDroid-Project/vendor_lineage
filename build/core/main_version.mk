d87f3f2365222c8ee48f61d48e4ba92383eacbc6
elpaablo <donopwhile1@gmail.com>
2023-01-31 11:38:25 +0000

    Clean build properties and bump alpha version to 1.1

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,12 +4,8 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# crDroid System Version
+# crDroid legal
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.crdroid.build.version=$(LINEAGE_VERSION) \
-    ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.crdroid.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(CR_VERSION) \
     ro.crdroidlegal.url=https://crdroid.net/legal.php
 
 # LineageOS Platform SDK Version
