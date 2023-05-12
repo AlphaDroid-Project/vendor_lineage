db13a95a4e19ce3edc2ff66d8acee0b4de56604c
elpaablo <donopwhile1@gmail.com>
2023-05-12 04:38:02 +0100

    createjson script: simplify file name processing and adapt to include optional revision version

--- build/tools/createjson.sh
+++ build/tools/createjson.sh
@@ -32,11 +32,7 @@ if [ -f $existingOTAjson ]; then
 	maintainer=`grep -m 1 -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
 	oem=`grep -m 1 -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
 	device=`grep -m 1 -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-	filename=$3
-	version=`echo "$3" | cut -d'-' -f6`
-	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
-	v_min=`echo "$version" | cut -d'.' -f2`
-	version=`echo $v_max.$v_min`
+	version=$(awk '{ sub(/v/, ""); sub(/\.zip/, ""); print }' <<< `echo "$3" | cut -d'-' -f6`)
 	buildprop=$2/system/build.prop
 	linenr=`grep -m 1 -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
 	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
@@ -119,11 +115,7 @@ if [ -f $existingOTAjson ]; then
 	]
 }' >> $output
 else
-	filename=$3
-	version=`echo "$3" | cut -d'-' -f6`
-	v_max=`echo "$version" | cut -d'.' -f1 | cut -d'v' -f2`
-	v_min=`echo "$version" | cut -d'.' -f2`
-	version=`echo $v_max.$v_min`
+	version=$(awk '{ sub(/v/, ""); sub(/\.zip/, ""); print }' <<< `echo "$3" | cut -d'-' -f6`)
 	buildprop=$2/system/build.prop
 	linenr=`grep -m 1 -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
 	timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
