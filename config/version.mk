f01a2ba9c2a78e1cb15fbc5716d3bb3ed91514c2
elpaablo <donopwhile1@gmail.com>
2023-06-23 12:18:01 +0200

    Update Alpha build type logic
    * Remove pixel framework flag. we're not tracking it.
    * Make GMS compat support apps optional on vanilla builds

--- config/version.mk
+++ config/version.mk
@@ -16,27 +16,46 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 endif
 
-
 # GAPPS_BUILD_TYPE
 # 0 - NO GAPPS (DEFAULT)
 # 1 - CORE GAPPS
 # 2 - FULL GAPPS
 
-WITH_GAPPS ?= 0
+# validate input from envsetup stored on GAPPS_BUILD_TYPE
 ifeq ($(filter $(strip $(GAPPS_BUILD_TYPE)),0 1 2),)
-GAPPS_BUILD_TYPE := $(strip $(WITH_GAPPS))
+# if GAPPS_BUILD_TYPE is empty or invalide, pick WITH_GAPPS value (if valid) from dt
+ifneq ($(filter $(strip $(WITH_GAPPS)),0 1 2),)
+GAPPS_BUILD_TYPE := $(WITH_GAPPS)
+else
+# default to vanilla if both WITH_GAPPS and GAPPS_BUILD_TYPE are invalid or undefined
+GAPPS_BUILD_TYPE := 0
 endif
-include vendor/gms/setup.mk
+endif
 
+ifeq ($(strip $(GAPPS_BUILD_TYPE)),0)
+ALPHA_BUILD_PACKAGE := vanilla
+ifeq ($(strip $(TARGET_INCLUDE_GMS_COMPAT)),true)
+# Optionally build GmsCompat and GrapheneApps on vanilla builds
+PRODUCT_PACKAGES += \
+    GmsCompat \
+    GmsCompatConfig \
+    GrapheneApps
+endif
+else
 ifeq ($(strip $(GAPPS_BUILD_TYPE)),1)
 ALPHA_BUILD_PACKAGE := core_gapps
 else
 ifeq ($(strip $(GAPPS_BUILD_TYPE)),2)
-ALPHA_BUILD_PACKAGE:= full_gapps
-else
-ALPHA_BUILD_PACKAGE := vanilla
+ALPHA_BUILD_PACKAGE := full_gapps
+# conditionally include pixel-framework
+#ifneq ($(strip $(TARGET_INCLUDE_PIXEL_FRAMEWORK)),false)
+#$(call inherit-product-if-exists, vendor/pixel-framework/config.mk)
+#endif
 endif
 endif
+endif
+# include GMS setup
+$(call inherit-product, vendor/gms/setup.mk)
 
 # Internal version
 LINEAGE_VERSION := Alpha$(ALPHA_VARIANT)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-$(ALPHA_BUILD_PACKAGE)-v$(ALPHA_VERSION)
