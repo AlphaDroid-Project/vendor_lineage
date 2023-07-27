248700764edfdac19fda2c623897f279296c028e
elpaablo <donopwhile1@gmail.com>
2023-07-27 05:15:36 +0200

    Face Unlock: add TARGET_FACE_UNLOCK_SUPPORTED check

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -64,6 +64,7 @@ PRODUCT_PACKAGES += \
     MatLog \
     OmniJaws
 
+ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
 ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
 PRODUCT_PACKAGES += \
     FaceUnlock
@@ -73,4 +74,5 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 endif
