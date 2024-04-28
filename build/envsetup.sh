92767e1af82e9414b76b9a5e02ccae266ccf7239
elpaablo <donopwhile1@gmail.com>
2024-04-28 13:13:27 +0000

    envsetup: make brunch variant default to user

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -81,7 +81,7 @@ function breakfast()
         else
             # This is probably just the Lineage model name
             if [ -z "$variant" ]; then
-                variant="userdebug"
+                variant="user"
             fi
 
             lunch lineage_$target-$aosp_target_release-$variant
