7ae79547b6e58b840f7cf4bfb451444ce75d83aa
elpaablo <donopwhile1@gmail.com>
2024-05-09 10:54:20 +0000

    Remove Matlog duplicate... again...

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -59,7 +59,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 PRODUCT_PACKAGES += \
     BatteryStatsViewer \
     GameSpace \
-    MatLog \
     OmniJaws \
     OmniStyle
 
