AlphaDroid-Project/vendor_lineage

commit dd4973bc47eff9364e524a811af906452ebf8452

Roomservice: adpapt setup to Alpha

build/tools/roomservice.py
@@ -41,10 +41,10 @@ except ImportError:
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/roomservice.xml"
-custom_default_revision =  "14.0"
-custom_dependencies = "crdroid.dependencies"
-org_manifest = "crdroidandroid"  # leave empty if org is provided in manifest
-org_display = "crDroid Android"  # needed for displaying
+custom_default_revision =  "alpha-14"
+custom_dependencies = "lineage.dependencies"
+org_manifest = "alphadroid"  # leave empty if org is provided in manifest
+org_display = "AlphaDroid"  # needed for displaying
 
 github_auth = None
 
@@ -286,7 +286,7 @@ def main():
         fallback_branch = detect_revision(repository)
         manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
         repo_path = "device/%s/%s" % (manufacturer, device)
-        adding = [{'repository': "crdroidandroid/" + repo_name, 'target_path': repo_path}]
+        adding = [{'repository': "alphadroid-devices/" + repo_name, 'target_path': repo_path}]
 
         add_to_manifest(adding, fallback_branch)
 

commit 35030db1f297fa7902b452d8f765224dccd0790e

roomservice: multiple fixes for AlphaDroid-devices adaptation
AlphaDroid has dedicated organization for devices, so set "org_manifest"
to "AlphaDroid-devices".

In that organization the device repositories are named both with
"android_device_" and with "device_" prefixes.
Let the roomservice know about this naming convenction.

While at it, improve dependent manifest preparation with unwiring
"alphadroid-devices" and just using ".full_name" from the JSON.

For ".full_name" example see the following:
$ curl -sL 'https://api.github.com/search/repositories?q=ginkgo+user:alphadroid-devices+in:name+fork:true' | jq '.items[].full_name'
"AlphaDroid-devices/vendor_xiaomi_ginkgo"
"AlphaDroid-devices/kernel_xiaomi_ginkgo"
"AlphaDroid-devices/device_xiaomi_ginkgo"

Fixes: dd4973bc47ef ("Roomservice: adpapt setup to Alpha")

Signed-off-by: Szabolcs Hubai <szab.hu@gmail.com>

build/tools/roomservice.py
@@ -43,7 +43,7 @@ DEBUG = False
 custom_local_manifest = ".repo/local_manifests/roomservice.xml"
 custom_default_revision =  "alpha-14"
 custom_dependencies = "lineage.dependencies"
-org_manifest = "alphadroid"  # leave empty if org is provided in manifest
+org_manifest = "alphadroid-devices"  # leave empty if org is provided in manifest
 org_display = "AlphaDroid"  # needed for displaying
 
 github_auth = None
@@ -278,15 +278,15 @@ def main():
     for repository in repositories:
         repo_name = repository['name']
 
-        if not (repo_name.startswith("android_device_") and
+        if not ((repo_name.startswith("android_device_") or repo_name.startswith("device_")) and
                 repo_name.endswith("_" + device)):
             continue
         print("Found repository: %s" % repository['name'])
 
         fallback_branch = detect_revision(repository)
-        manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
+        manufacturer = repo_name.replace("android_device_", "").replace("device_", "").replace("_" + device, "")
         repo_path = "device/%s/%s" % (manufacturer, device)
-        adding = [{'repository': "alphadroid-devices/" + repo_name, 'target_path': repo_path}]
+        adding = [{'repository': repository["full_name"], 'target_path': repo_path}]
 
         add_to_manifest(adding, fallback_branch)
 