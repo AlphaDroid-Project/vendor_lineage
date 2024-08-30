93a9ea3ca17cbe287766a237e7e23d96744d6684
ShevT <evgeny.shishkov@gmail.com>
2024-08-30 12:38:33 +0300

    crdroid: Adding an option to build DeviceAsWebcam

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -74,3 +74,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 endif
+
+# DeviceAsWebcam
+ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
+    PRODUCT_PACKAGES += \
+        DeviceAsWebcam
+
+    PRODUCT_VENDOR_PROPERTIES += \
+        ro.usb.uvc.enabled=true
+endif
