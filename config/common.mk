ce0968d8b80814a8c7cb6390d54f5a884f72bff4
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-17 01:45:38 +0800

    config: common: Build nano_recovery
    Change-Id: I656ba63c7b137d7a6225e84b153e07d92e8ecb9d

--- config/common.mk
+++ config/common.mk
@@ -149,6 +149,9 @@ PRODUCT_PACKAGES += \
     setcap \
     vim
 
+PRODUCT_PACKAGES += \
+    nano_recovery
+
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/curl \
     system/bin/getcap \
