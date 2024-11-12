58d303d7f4c0f27720df2d509c0aaf001d46dc68
elpaablo <donopwhile1@gmail.com>
2024-11-12 09:06:30 +0000

    Add option to enable adb via device tree

--- config/common.mk
+++ config/common.mk
@@ -26,14 +26,19 @@ endif
 endif
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
-# Disable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+  # Disable ADB authentication
+  PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 else
-# Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+  ifeq ($(TARGET_ENABLE_ADB),true)
+    # Disable ADB authentication and enable adb mode
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0 persist.sys.usb.config=mtp,adb
+  else
+    # Enable ADB authentication
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 
-# Disable extra StrictMode features on all non-engineering builds
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+    # Disable extra StrictMode features on all non-engineering builds
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+  endif
 endif
 
 # Backup Tool
